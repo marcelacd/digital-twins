5c4af8fb3476698f3a199ba533562b194b467300
Marcela Carmona <gloriamarcela.carmonaduran@emeal.nttdata.com>
2024-09-24 08:54:59 -0500

    Añadir nevos cambios

--- src/services/athenaService.js
+++ src/services/athenaService.js
@@ -1,59 +1,3 @@
-// import { StartQueryExecutionCommand, GetQueryExecutionCommand, GetQueryResultsCommand } from '@aws-sdk/client-athena';
-// import client from './athenaClient';
-
-// export const executeAthenaQuery = async (query) => {
-//     console.log(query)
-//     try {
-//         // Iniciar la consulta en Athena
-//         const startQueryCommand = new StartQueryExecutionCommand({
-//             QueryString: query,
-//             QueryExecutionContext: { Database: 'digital-twins-nutresa-glue-db' },
-//             ResultConfiguration: {
-//                 OutputLocation: 's3://digital-twins-nutresa-s3/metadata/',
-//             },
-//         });
-
-//         const queryExecution = await client.send(startQueryCommand);
-//         const queryExecutionId = queryExecution.QueryExecutionId;
-
-//         let queryStatus = 'RUNNING';
-
-//         while (queryStatus === 'RUNNING' || queryStatus === 'QUEUED') {
-//             console.log('Checking query status:', queryStatus);
-//             const getQueryExecutionCommand = new GetQueryExecutionCommand({ QueryExecutionId: queryExecutionId });
-//             const queryExecutionStatus = await client.send(getQueryExecutionCommand);
-//             queryStatus = queryExecutionStatus.QueryExecution.Status.State;
-
-//             if (queryStatus === 'FAILED' || queryStatus === 'CANCELLED') {
-//                 throw new Error('La consulta falló o fue cancelada.');
-//             }
-
-//             // Espera más tiempo si está en estado QUEUED
-//             const waitTime = queryStatus === 'QUEUED' ? 5000 : 2000;
-//             await new Promise((resolve) => setTimeout(resolve, waitTime));
-//         }
-
-//         if (queryStatus === 'SUCCEEDED') {
-//             const getQueryResultsCommand = new GetQueryResultsCommand({ QueryExecutionId: queryExecutionId });
-//             const queryResultsResponse = await client.send(getQueryResultsCommand);
-
-//             // Procesar los resultados y devolver los datos
-//             const results = queryResultsResponse.ResultSet.Rows.map(row =>
-//                 row.Data.map(col => col.VarCharValue || '')
-//             );
-//             console.log(results)
-//             console.log(JSON.stringify(results))
-//             return results;
-//         }
-//     } catch (error) {
-//         console.log(error)
-//         // zona, venta_neta_acum_ano_actual_eco, venta_neta_acum_ano_actual_kg, venta_neta_acum_ano_actual_un
-
-//         console.error('Error ejecutando la consulta Athena:', error);
-//         return undefined
-//     }
-// };
-
 import { 
     StartQueryExecutionCommand, 
     GetQueryExecutionCommand, 
@@ -87,7 +31,7 @@ export const executeAthenaQuery = async (query) => {
                 throw new Error('La consulta falló o fue cancelada.');
             }
 
-            const waitTime = queryStatus === 'QUEUED' ? 5000 : 2000;
+            const waitTime = queryStatus === 'QUEUED' ? 500 : 500;
             await new Promise((resolve) => setTimeout(resolve, waitTime));
         }
 
